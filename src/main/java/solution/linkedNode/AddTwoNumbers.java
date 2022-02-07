fa50f2d345a9dca0da49f8eace2dd818f688fd23
zhoubao <zhoubao@meicai.cn>
2022-02-07 17:16:45 +0800

    2. 两数相加 - 链表 进位标识

---
+++ src/main/java/solution/linkedNode/AddTwoNumbers.java
@@ -0,0 +1,62 @@
+package solution.linkedNode;
+
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * <p>
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhoubao
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while (null != l1 || null != l2 || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+            // 进位处理
+            carry = sumVal / 10;
+            ListNode sumNode = new ListNode(sumVal % 10);
+            // 设置当前节点的下一个节点值为计算的和
+            cursor.next = sumNode;
+            // 游标游动至下一个节点
+            cursor = cursor.next;
+            if (null != l1) {
+                l1 = l1.next;
+            }
+            if (null != l2) {
+                l2 = l2.next;
+            }
+        }
+        return root.next;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
+
+
